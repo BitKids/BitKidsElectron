692335f87350b8a124f6e07a89fa5f2a9a5616f8
Toshinori Minami <toshinoriminami@gmail.com>
2016-03-28 00:45:21 +0900

    Implement DevTools

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,6 +9,14 @@ export default class App extends Component {
     return (
       <div>
         {this.props.children}
+        {
+          (() => {
+            if (process.env.NODE_ENV !== 'production') {
+              const DevTools = require('./DevTools').default;
+              return <DevTools />;
+            }
+          })()
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
-import { createStore } from 'redux';
+import configureStore from './store/configureStore';
 import routes from './routes';
-import rootReducer from './reducers'
 
-const store = createStore(rootReducer);
+const store = configureStore();
 const rootElement = document.getElementById('app');
 
 render(
